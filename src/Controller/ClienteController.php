59f667b5cc8f648578bb26391df531e1f954afb0
Alexandre S Pereira <alexandresp15@gmail.com>
2020-10-27 09:01:26 -0300

    Refactoring project

--- src/Controller/ClienteController.php
+++ src/Controller/ClienteController.php
@@ -11,24 +11,24 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/cliente")
+ * @Route("/")
  */
 class ClienteController extends AbstractController
 {
     /**
-     * @Route("/", name="cliente_index", methods={"GET"})
+     * @Route("/", name="cliente_listar", methods={"GET"})
      */
-    public function index(ClienteRepository $clienteRepository): Response
+    public function listar(ClienteRepository $clienteRepository): Response
     {
-        return $this->render('cliente/index.html.twig', [
+        return $this->render('cliente/listar.html.twig', [
             'clientes' => $clienteRepository->findAll(),
         ]);
     }
 
     /**
-     * @Route("/new", name="cliente_new", methods={"GET","POST"})
+     * @Route("/criar", name="cliente_criar", methods={"GET","POST"})
      */
-    public function new(Request $request): Response
+    public function criar(Request $request): Response
     {
         $cliente = new Cliente();
         $form = $this->createForm(ClienteType::class, $cliente);
@@ -39,29 +39,29 @@ class ClienteController extends AbstractController
             $entityManager->persist($cliente);
             $entityManager->flush();
 
-            return $this->redirectToRoute('cliente_index');
+            return $this->redirectToRoute('cliente_listar');
         }
 
-        return $this->render('cliente/new.html.twig', [
+        return $this->render('cliente/criar.html.twig', [
             'cliente' => $cliente,
             'form' => $form->createView(),
         ]);
     }
 
     /**
-     * @Route("/{id}", name="cliente_show", methods={"GET"})
+     * @Route("/{id}", name="cliente_mostrar", methods={"GET"})
      */
-    public function show(Cliente $cliente): Response
+    public function mostrar(Cliente $cliente): Response
     {
-        return $this->render('cliente/show.html.twig', [
+        return $this->render('cliente/mostrar.html.twig', [
             'cliente' => $cliente,
         ]);
     }
 
     /**
-     * @Route("/{id}/edit", name="cliente_edit", methods={"GET","POST"})
+     * @Route("/{id}/editar", name="cliente_editar", methods={"GET","POST"})
      */
-    public function edit(Request $request, Cliente $cliente): Response
+    public function editar(Request $request, Cliente $cliente): Response
     {
         $form = $this->createForm(ClienteType::class, $cliente);
         $form->handleRequest($request);
@@ -69,19 +69,19 @@ class ClienteController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('cliente_index');
+            return $this->redirectToRoute('cliente_listar');
         }
 
-        return $this->render('cliente/edit.html.twig', [
+        return $this->render('cliente/editar.html.twig', [
             'cliente' => $cliente,
             'form' => $form->createView(),
         ]);
     }
 
     /**
-     * @Route("/{id}", name="cliente_delete", methods={"DELETE"})
+     * @Route("/{id}", name="cliente_deletar", methods={"DELETE"})
      */
-    public function delete(Request $request, Cliente $cliente): Response
+    public function deletar(Request $request, Cliente $cliente): Response
     {
         if ($this->isCsrfTokenValid('delete'.$cliente->getId(), $request->request->get('_token'))) {
             $entityManager = $this->getDoctrine()->getManager();
@@ -89,6 +89,6 @@ class ClienteController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('cliente_index');
+        return $this->redirectToRoute('cliente_listar');
     }
 }
